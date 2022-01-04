005f3b2f91b902ac75ffe57bbd5ae66e4d76f066
salindae25 <salindae25@gmail.com>
2022-01-04 20:21:03 +0530

    [Button component]I can see different button types: default, outline and text

---
+++ src/lib/Button.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let disableShadow = false;
+  export let variant = "";
+</script>
+
+<button
+  class="default"
+  class:disable={disableShadow}
+  class:outline={variant === "outline"}
+  class:text={variant === "text"}
+>
+  <slot name="icon" />
+  <slot name="title">Defalt</slot>
+</button>
+
+<style>
+  .default {
+    width: 81px;
+    height: 36px;
+    font-family: Noto Sans JP;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+
+    color: #3f3f3f;
+    background: #e0e0e0;
+    box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+    border-radius: 6px;
+  }
+  .disable {
+    @apply text-white bg-blue-600;
+  }
+  .outline {
+    @apply border border-blue-600 text-blue-600 bg-white outline-none;
+  }
+  .text {
+    @apply border-0 text-blue-600 bg-white outline-none shadow-none;
+  }
+</style>
---
+++ src/routes/buttons.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Button from "$lib/Button.svelte";
+</script>
+
+<div class="flex w-full  flex-col">
+  <h2 class="font-Poppins text-2xl text-[hsl(0,0%,31%)]">Buttons</h2>
+  <div class="grid grid-cols-2 mt-7 gap-y-12 gap-x-12 justify-between">
+    <article class="flex flex-col gap-2">
+      <div class="font-Poppins text-xs">
+        {`<Button />`}
+      </div>
+      <Button />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-Poppins text-xs">
+        {`&:hover, &:focus`}
+      </div>
+      <Button />
+    </article>
+
+    <article class="flex flex-col gap-2">
+      <div class="font-Poppins text-xs">
+        {'<Button variant="outline" />'}
+      </div>
+      <Button variant="outline" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-Poppins text-xs">
+        {`&:hover, &:focus`}
+      </div>
+      <Button variant="outline" />
+    </article>
+
+    <article class="flex flex-col gap-2">
+      <div class="font-Poppins text-xs">
+        {'<Button variant="text" />'}
+      </div>
+      <Button variant="text" />
+    </article>
+
+    <article class="flex flex-col gap-2">
+      <div class="font-Poppins text-xs">
+        {`&:hover, &:focus`}
+      </div>
+      <Button variant="text" />
+    </article>
+
+    <article class="flex flex-col gap-2">
+      <div class="font-Poppins text-xs">
+        {"<Button disableShadow />"}
+      </div>
+      <Button disableShadow />
+    </article>
+  </div>
+</div>
