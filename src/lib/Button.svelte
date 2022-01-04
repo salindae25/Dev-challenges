4f324aa27f34671500f9f1df9a0717e7793b8432
salindae25 <salindae25@gmail.com>
2022-01-04 22:38:41 +0530

    [Button component]When I hover or focus, I can see visual indicators

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -6,9 +6,26 @@
   export let endIcon = "";
   export let title = "Defalt";
   export let size = "md";
+  export let color = "primary";
+  export let hover = false;
+  const id = `button-${uuid()}`;
+  function uuid() {
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+      s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+  }
 </script>
 
 <button
+  {id}
   class="default"
   class:disableShadow
   class:disabled
@@ -17,7 +34,13 @@
   class:size-sm={size === "sm"}
   class:size-md={size === "md"}
   class:size-lg={size === "lg"}
+  class:color-default={color === "default"}
+  class:color-primary={color === "primary"}
+  class:color-secondary={color === "secondary"}
+  class:color-danger={color === "danger"}
+  class:hover
   disabled
+  {...$$props}
 >
   {#if startIcon !== ""}
     <span class="material-icons w-3 text-base leading-4"> {startIcon} </span>
@@ -42,17 +65,32 @@
   }
   .disabled {
     @apply shadow-none;
-    background-color: hsla(0, 0%, 88%, 1);
-    color: hsla(0, 0%, 62%, 1);
+    background-color: hsla(0, 0%, 88%, 1) !important;
+    color: hsla(0, 0%, 62%, 1) !important;
   }
   .disableShadow {
     @apply shadow-none;
   }
   .outline {
-    @apply border border-blue-600 text-blue-600 bg-white outline-none;
+    @apply outline-none;
+    color: hsla(231, 99%, 62%, 1) !important;
+    background: #fff !important;
+    border: 1px solid hsla(231, 99%, 62%, 1);
+  }
+  .outline:hover,
+  .outline:hover:focus,
+  .outline.hover {
+    background: rgba(41, 98, 255, 0.1) !important;
   }
   .text {
-    @apply border-0 text-blue-600 bg-white outline-none shadow-none;
+    @apply border-0  outline-none shadow-none;
+    color: hsla(231, 99%, 62%, 1) !important;
+    background: #fff !important;
+  }
+  .text:hover,
+  .text:hover:focus,
+  .text.hover {
+    background: rgba(41, 98, 255, 0.1) !important;
   }
   .disabled.text {
     color: hsla(0, 0%, 62%, 1) !important;
@@ -65,5 +103,59 @@
   }
   .size-lg {
     padding: 11px 22px;
+  }
+  .color-default {
+    background: #e0e0e0;
+    color: #3f3f3f;
+  }
+  .color-default:hover,
+  .color-default:hover:focus,
+  .color-default.hover {
+    background: #aeaeae;
+  }
+  .outline.color-default {
+    border-color: #3f3f3f !important;
+    color: #3f3f3f !important;
+  }
+  .text.color-default {
+    color: #3f3f3f !important;
+  }
+  .color-primary {
+    @apply text-white bg-blue-600;
+  }
+  .color-primary:hover,
+  .color-primary:hover:focus,
+  .color-primary.hover {
+    background: #0039cb;
+  }
+  .color-secondary {
+    background: #455a64;
+  }
+  .color-secondary:hover,
+  .color-secondary:hover:focus,
+  .color-secondary.hover {
+    background: #1c313a;
+  }
+  .outline.color-secondary {
+    border-color: #455a64 !important;
+    color: #455a64 !important;
+  }
+  .text.color-secondary {
+    color: #455a64 !important;
+  }
+  .color-danger {
+    background: #d32f2f;
+  }
+  .color-danger:hover,
+  .color-danger:focus,
+  .color-danger.hover {
+    background: #9a0007;
+  }
+  .outline.color-danger {
+    border-color: #d32f2f !important;
+    color: #d32f2f !important;
+  }
+  .text.color-danger {
+    color: #d32f2f !important;
   }
 </style>
--- src/routes/buttons.svelte
+++ src/routes/buttons.svelte
@@ -4,99 +4,99 @@
 
 <div class="flex w-full  flex-col">
   <h2 class="font-Poppins text-2xl text-[hsl(0,0%,31%)]">Buttons</h2>
-  <div class="grid grid-cols-2 mt-7 gap-y-12 gap-x-12 justify-between">
+  <div class="grid grid-cols-2 mt-7 gap-y-5 gap-x-12 justify-between">
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {`<Button />`}
       </div>
       <Button />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {`&:hover, &:focus`}
       </div>
-      <Button />
+      <Button hover />
     </article>
 
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {'<Button variant="outline" />'}
       </div>
       <Button variant="outline" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {`&:hover, &:focus`}
       </div>
-      <Button variant="outline" />
+      <Button variant="outline" hover />
     </article>
 
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {'<Button variant="text" />'}
       </div>
       <Button variant="text" />
     </article>
 
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {`&:hover, &:focus`}
       </div>
-      <Button variant="text" />
+      <Button variant="text" hover />
     </article>
 
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {"<Button disableShadow />"}
       </div>
       <Button disableShadow />
     </article>
     <article class="flex flex-col gap-2" />
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {"<Button disabled />"}
       </div>
       <Button disabled />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {"<Button variant='text' disabled />"}
       </div>
       <Button variant="text" disabled />
     </article>
 
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {" <Button startIcon='local_grocery_store' />"}
       </div>
       <Button startIcon="local_grocery_store" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {" <Button endIcon='local_grocery_store' />"}
       </div>
       <Button endIcon="local_grocery_store" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {" <Button variant='text' startIcon='local_grocery_store' />"}
       </div>
       <Button variant="text" startIcon="local_grocery_store" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {" <Button variant='text' endIcon='local_grocery_store' />"}
       </div>
       <Button variant="text" endIcon="local_grocery_store" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {" <Button variant='outline' startIcon='local_grocery_store' />"}
       </div>
       <Button variant="outline" startIcon="local_grocery_store" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {" <Button variant='outline' endIcon='local_grocery_store' />"}
       </div>
       <Button variant="outline" endIcon="local_grocery_store" />
@@ -104,22 +104,131 @@
   </div>
   <div class="grid grid-cols-3 mt-7 gap-y-12 gap-x-12 justify-between">
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {`<Button size="sm"/>`}
       </div>
       <Button size="sm" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {`<Button  size="md"/>`}
       </div>
       <Button size="md" />
     </article>
     <article class="flex flex-col gap-2">
-      <div class="font-Poppins text-xs">
+      <div class="font-ubuntu-mono text-xs">
         {`<Button size="lg"/>`}
       </div>
       <Button size="lg" />
     </article>
+  </div>
+  <div class="grid grid-flow-col mt-7 gap-y-12 gap-x-12 justify-between">
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="default"/>`}
+      </div>
+      <Button color="default" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button  color="primary"/>`}
+      </div>
+      <Button color="primary" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="secondary"/>`}
+      </div>
+      <Button color="secondary" title="Secondary" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="danger"/>`}
+      </div>
+      <Button color="danger" title="Danger" />
+    </article>
+  </div>
+  <div class="grid grid-flow-col mt-7 gap-y-12 gap-x-12 justify-between">
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`&:hover, &:focus`}
+      </div>
+      <Button color="default" hover />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`&:hover, &:focus`}
+      </div>
+      <Button color="primary" hover />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`&:hover, &:focus`}
+      </div>
+      <Button color="secondary" title="Secondary" hover />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`&:hover, &:focus`}
+      </div>
+      <Button color="danger" title="Danger" hover />
+    </article>
+  </div>
+  <div class="grid grid-flow-col mt-7 gap-y-12 gap-x-12 justify-between">
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="default" variant="text"/>`}
+      </div>
+      <Button color="default" variant="text" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button  color="primary" variant="text" />`}
+      </div>
+      <Button color="primary" variant="text" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="secondary" variant="text" />`}
+      </div>
+      <Button color="secondary" title="Secondary" variant="text" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="danger" variant="text" />`}
+      </div>
+      <Button color="danger" title="Danger" variant="text" />
+    </article>
+  </div>
+  <div class="grid grid-flow-col mt-7 gap-y-12 gap-x-12 justify-between">
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="default" variant="outline"/>`}
+      </div>
+      <Button color="default" variant="outline" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button  color="primary" variant="outline" />`}
+      </div>
+      <Button color="primary" variant="outline" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="secondary" variant="outline" />`}
+      </div>
+      <Button color="secondary" title="Secondary" variant="outline" />
+    </article>
+    <article class="flex flex-col gap-2">
+      <div class="font-ubuntu-mono text-xs">
+        {`<Button color="danger" variant="outline" />`}
+      </div>
+      <Button color="danger" title="Danger" variant="outline" />
+    </article>
+  </div>
+  <div class="grid grid-flow-col mt-7 gap-y-12 gap-x-12 justify-between">
+    <span class="text-gray-500"
+      >created by <b>salindae25</b> - devChallenge</span
+    >
   </div>
 </div>
