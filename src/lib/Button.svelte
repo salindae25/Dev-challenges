7c4a4cfc3e505195b9d393209c1d86ea96742ed1
salindae25 <salindae25@gmail.com>
2022-01-06 21:42:17 +0530

    [Input component] I can see error state

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { uuid } from "$lib/Util";
+
   export let disableShadow = false;
   export let disabled = false;
   export let variant = "";
@@ -9,19 +11,6 @@
   export let color = "primary";
   export let hover = false;
   const id = `button-${uuid()}`;
-  function uuid() {
-    var s = [];
-    var hexDigits = "0123456789abcdef";
-    for (var i = 0; i < 36; i++) {
-      s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
-    }
-    s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
-    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
-    s[8] = s[13] = s[18] = s[23] = "-";
-
-    var uuid = s.join("");
-    return uuid;
-  }
 </script>
 
 <button
---
+++ src/lib/Input.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import Button from "$lib/Button.svelte";
+  import { uuid } from "$lib/Util";
+
+  export let error = false;
+  export let hover = false;
+  export let focus = false;
+  export let disabled = false;
+  export let label = "Label";
+  export let placeholder = "Placeholder";
+  export let id = `input-${uuid()}`;
+</script>
+
+<div class="container">
+  <input
+    {id}
+    class="default pl-3 py-[18px] mt-1 placeholder:text-[#828282] font-Noto-sans font-medium"
+    class:hover
+    class:focus
+    class:error
+    class:disabled
+    {placeholder}
+    {disabled}
+  />
+  <label for={id}>{label}</label>
+</div>
+
+<style lang="scss">
+  .container {
+    @apply flex flex-col-reverse;
+  }
+  .default {
+    min-width: 200px;
+    font-size: 14px;
+    border: 1px solid #828282;
+    box-sizing: border-box;
+    border-radius: 8px;
+    color: #333333;
+    outline: none;
+    &.hover,
+    &:hover {
+      @apply border-[#333333];
+      & ~ label {
+        color: #333333;
+      }
+    }
+
+    &.focus,
+    &:focus {
+      @apply border-[#2962FF];
+      & ~ label {
+        color: #2962ff;
+      }
+    }
+  }
+  .error {
+    border-color: #d32f2f;
+    & ~ label {
+      color: #d32f2f;
+    }
+    &.hover,
+    &:hover {
+      @apply border-[#333333];
+      & ~ label {
+        color: #333333;
+      }
+    }
+
+    &.focus,
+    &:focus {
+      @apply border-[#D32F2F];
+      & ~ label {
+        color: #d32f2f;
+      }
+    }
+  }
+  .disabled {
+    background-color: #f2f2f2;
+    border-color: #e0e0e0;
+    &.hover,
+    &:hover {
+      @apply border-[#e0e0e0];
+      & ~ label {
+        color: #333333;
+      }
+    }
+
+    &.focus,
+    &:focus {
+      @apply border-[#e0e0e0];
+      & ~ label {
+        color: #333333;
+      }
+    }
+  }
+</style>
